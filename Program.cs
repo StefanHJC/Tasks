9dc427d3ef047cf64ecc067fde3dff5eb3d6ba9f
Stefan <flieger-luf@yandex.ru>
2022-05-17 01:27:31 +0300

    Task3

--- Program.cs
+++ Program.cs
@@ -1,40 +1,87 @@
-﻿using System;
-using System.IO;
- 
-namespace Lesson
+﻿namespace Lesson
 {
     class Program
     {
         static void Main(string[] args)
         {
- 
+            var pathfinder1 = new Pathfinder(ChainOfLoggers.Create(new FileLogWritter()));
+            var pathfinder2 = new Pathfinder(ChainOfLoggers.Create(new ConsoleLogWritter()));
+            var pathfinder3 = new Pathfinder(ChainOfLoggers.Create(new SecureLogWritter(new FileLogWritter())));
+            var pathfinder4 = new Pathfinder(ChainOfLoggers.Create(new SecureLogWritter(new ConsoleLogWritter())));
+            var pathfinder5 = new Pathfinder(ChainOfLoggers.Create(new ConsoleLogWritter(), new SecureLogWritter(new FileLogWritter())));
         }
     }
- 
-    class ConsoleLogWritter
+
+    interface ILogger
+    {
+        public abstract void WriteError(string message);
+    }
+
+    class ConsoleLogWritter : ILogger
     {
         public virtual void WriteError(string message)
         {
             Console.WriteLine(message);
         }
     }
- 
-    class FileLogWritter
+
+    class FileLogWritter : ILogger
     {
         public virtual void WriteError(string message)
         {
             File.WriteAllText("log.txt", message);
         }
     }
- 
-    class SecureConsoleLogWritter : ConsoleLogWritter
+
+    class SecureLogWritter : ILogger
     {
-        public override void WriteError(string message)
+        private ILogger _baseLogger;
+
+        public SecureLogWritter(ILogger baseLogger)
+        {
+            _baseLogger = baseLogger;
+        }
+
+        public virtual void WriteError(string message)
         {
             if (DateTime.Now.DayOfWeek == DayOfWeek.Friday)
-            {
-                base.WriteError(message);
-            }
+                _baseLogger.WriteError(message);
+        }
+    }
+
+    class ChainOfLoggers : ILogger
+    {
+        private IEnumerable<ILogger> _loggers;
+
+        public ChainOfLoggers(IEnumerable<ILogger> loggers)
+        {
+            _loggers = loggers;
+        }
+
+        public void WriteError(string message)
+        {
+            foreach (var logger in _loggers)
+                logger.WriteError(message);
+        }
+
+        public static ChainOfLoggers Create(params ILogger[] loggers)
+        {
+            return new ChainOfLoggers(loggers);
+        }
+    }
+
+    class Pathfinder
+    {
+        private ChainOfLoggers _loggers;
+
+        public Pathfinder(ChainOfLoggers loggers)
+        {
+            _loggers = loggers;
+        }
+
+        public void Find()
+        {
+            _loggers.WriteError("гыыыы");
         }
     }
 }
