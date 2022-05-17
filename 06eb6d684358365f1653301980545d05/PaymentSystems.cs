4b4f8b9dd8cd74b4ad5645f5de1bd9f609639810
Stefan <flieger-luf@yandex.ru>
2022-05-17 23:10:51 +0300

    Task4

--- 06eb6d684358365f1653301980545d05/PaymentSystems.cs
+++ 06eb6d684358365f1653301980545d05/PaymentSystems.cs
@@ -1,11 +1,18 @@
+using System.Security.Cryptography;
+using System.Text;
+
 class Program
 {
     static void Main(string[] args)
     {
-        //Выведите платёжные ссылки для трёх разных систем платежа: 
-        //pay.system1.ru/order?amount=12000RUB&hash={MD5 хеш ID заказа}
-        //order.system2.ru/pay?hash={MD5 хеш ID заказа + сумма заказа}
-        //system3.com/pay?amount=12000&curency=RUB&hash={SHA-1 хеш сумма заказа + ID заказа + секретный ключ от системы}
+        var paymentSystem1 = new FirstPaymentSystem();
+        var paymentSystem2 = new SecondPaymentSystem();
+        var paymentSystem3 = new ThirdPaymentSystem();
+        var order = new Order(1, 12000);
+
+        Console.WriteLine(paymentSystem1.GetPayingLink(order));
+        Console.WriteLine(paymentSystem2.GetPayingLink(order));
+        Console.WriteLine(paymentSystem3.GetPayingLink(order));
     }
 }
 
@@ -24,5 +31,48 @@ public interface IPaymentSystem
 
 public class FirstPaymentSystem : IPaymentSystem
 {
+    public virtual string GetPayingLink(Order order)
+    {
+        string hash = ComputeMD5(order.Amount.ToString());
 
+        return $"pay.system1.ru/order?amount={order.Amount}RUB&hash={hash}";
+    }
+
+    protected string ComputeMD5(string value)
+    {
+        var md5 = MD5.Create();
+        byte[] hashBytes = md5.ComputeHash(Encoding.UTF8.GetBytes(value));
+        string hash = Convert.ToBase64String(hashBytes);
+
+        return hash;
+    }
+}
+
+public class SecondPaymentSystem : FirstPaymentSystem
+{
+    public override string GetPayingLink(Order order)
+    {
+        string hash = ComputeMD5((order.Id + order.Amount).ToString());
+
+        return $"order.system2.ru/pay?hash={hash}";
+    }
+}
+
+public class ThirdPaymentSystem : IPaymentSystem
+{
+    public string GetPayingLink(Order order)
+    {
+        var sha1 = SHA1.Create();
+        string secretKey = GetSecretKey();
+
+        byte[] hashBytes = sha1.ComputeHash(Encoding.UTF8.GetBytes((order.Amount + order.Id).ToString() + secretKey));
+        string hash = Convert.ToBase64String(hashBytes);
+
+        return $"system3.com/pay?amount={order.Amount}&curency=RUB&hash={hash}";
+    }
+
+    private string GetSecretKey()
+    {
+        return Console.ReadLine();
+    }
 }
