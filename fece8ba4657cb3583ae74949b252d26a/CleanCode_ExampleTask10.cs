bebc37ce94ce41cfb996847710f5d22cb84d3152
Stefan <flieger-luf@yandex.ru>
2022-05-19 02:07:32 +0300

    Task7

--- fece8ba4657cb3583ae74949b252d26a/CleanCode_ExampleTask10.cs
+++ fece8ba4657cb3583ae74949b252d26a/CleanCode_ExampleTask10.cs
@@ -1,8 +1,19 @@
 class Weapon
 {
     private int _bullets;
+    private readonly int _maxBullets;
+    private readonly int _minBullets;
+    private readonly int _bulletsPerShot;
 
-    public bool CanShoot() => _bullets > 0;
+    public Weapon(int maxBullets, int bulletsPerShot)
+    {
+        _maxBullets = maxBullets;
+        _bulletsPerShot = bulletsPerShot;
+        _minBullets = bulletsPerShot;
+        _bullets = maxBullets;
+    }
 
-    public void Shoot() => _bullets -= 1;
+    public bool CanShoot() => _bullets > _minBullets;
+
+    public void Shoot() => _bullets -= _bulletsPerShot;
 }
