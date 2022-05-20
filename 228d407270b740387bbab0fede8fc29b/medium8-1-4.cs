3e4ae5d5e78cdd60f31a8c255362438e080af958
Stefan <flieger-luf@yandex.ru>
2022-05-21 02:49:22 +0300

    Task12

--- 228d407270b740387bbab0fede8fc29b/medium8-1-4.cs
+++ 228d407270b740387bbab0fede8fc29b/medium8-1-4.cs
@@ -1,12 +1,20 @@
 class Player
 {
-    public string Name { get; private set; }
-    public float WeaponCooldown { get; private set; }
-    public float MovementDirectionX { get; private set; }
-    public float MovementDirectionY { get; private set; }
-    public int Age { get; private set; }
-    public int WeaponDamage { get; private set; }
+    private Weapon _weapon;
+
+    public Vector2 MovementDirection { get; private set; }
     public float MovementSpeed { get; private set; }
+    public string Name { get; private set; }
+    public int Age { get; private set; }
+    public bool IsReloading => _weapon.Bullets == 0;
+
+    public Player(Weapon weapon, string name, float movementSpeed, int age)
+    {
+        _weapon = weapon;
+        Name = name;
+        MovementSpeed = movementSpeed;
+        Age = age;
+    }
 
     public void Move()
     {
@@ -17,9 +25,37 @@ class Player
     {
         //attack
     }
+}
 
-    public bool IsReloading()
+class Weapon
+{
+    public float Damage { get; private set; }
+    public float ReloadingTime { get; private set; }
+    public int Bullets { get; private set; }
+    public int MaxBullets { get; private set; }
+
+    public Weapon(float damage, float reloadingTime, int maxBullets)
     {
-        throw new NotImplementedException();
+        Damage = damage;
+        ReloadingTime = reloadingTime;
+        MaxBullets = maxBullets;
+        Bullets = maxBullets;
+    }
+
+    public void Shoot()
+    {
+        // make shot
+    }
+}
+
+struct Vector2
+{
+    public float X;
+    public float Y;
+
+    public Vector2(float x, float y)
+    {
+        X = x;
+        Y = y;
     }
 }
