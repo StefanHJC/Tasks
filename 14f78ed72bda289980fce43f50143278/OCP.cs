5eef5084a15489ec087eba2711a1912fc6fc6600
Stefan <flieger-luf@yandex.ru>
2022-05-22 18:00:50 +0300

    Task14

--- 14f78ed72bda289980fce43f50143278/OCP.cs
+++ 14f78ed72bda289980fce43f50143278/OCP.cs
@@ -1,24 +1,106 @@
-using System;
-
 namespace IMJunior
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             var orderForm = new OrderForm();
             var paymentHandler = new PaymentHandler();
+            var paymentSystemFactory = new PaymentSystemFactory();
 
             var systemId = orderForm.ShowForm();
 
-            if (systemId == "QIWI")
-                Console.WriteLine("Перевод на страницу QIWI...");
-            else if (systemId == "WebMoney")
-                Console.WriteLine("Вызов API WebMoney...");
-            else if (systemId == "Card")
-                Console.WriteLine("Вызов API банка эмитера карты Card...");
+            IPaymentSystem paymentSystem = paymentSystemFactory.CreatePaymentSystem(systemId);
+            paymentHandler.ShowPaymentResult(paymentSystem);
+        }
+    }
 
-            paymentHandler.ShowPaymentResult(systemId);
+    public interface IPaymentSystem
+    {
+        public void ExecutePayment();
+
+        public bool IsPaymentExecuted();
+    }
+
+    public class PaymentSystemFactory
+    {
+        private IPaymentSystem _paymentSystem;
+
+        public IPaymentSystem CreatePaymentSystem(string paymentSystem)
+        {
+            switch (paymentSystem)
+            {
+                case "QIWI":
+                    _paymentSystem = new QIWI();
+                    break;
+
+                case "Webmoney":
+                    _paymentSystem = new Webmoney();
+                    break;
+
+                case "Card":
+                    _paymentSystem = new Card();
+                    break;
+
+                case "YobaPay":
+                    _paymentSystem = new YobaPay();
+                    break;
+                default:
+                    throw new ArgumentException();
+            }
+            return _paymentSystem;
+        }
+    }
+
+    public class QIWI : IPaymentSystem
+    {
+        public void ExecutePayment()
+        {
+            Console.WriteLine("Перевод на страницу QIWI...");
+        }
+
+        public bool IsPaymentExecuted()
+        {
+            return true;
+        }
+    }
+
+    public class Webmoney : IPaymentSystem
+    {
+        public void ExecutePayment()
+        {
+            Console.WriteLine("Вызов API WebMoney...");
+        }
+
+        public bool IsPaymentExecuted()
+        {
+            return true;
+        }
+    }
+
+    public class Card : IPaymentSystem
+    {
+        public void ExecutePayment()
+        {
+            Console.WriteLine("Вызов API банка эмитера карты Card...");
+        }
+
+        public bool IsPaymentExecuted()
+        {
+            return true;
+        }
+    }
+
+    public class YobaPay : IPaymentSystem
+    {
+        public void ExecutePayment()
+        {
+            Console.WriteLine("ALLO YOBA ETO TI?");
+        }
+
+        public bool IsPaymentExecuted()
+        {
+            return true;
         }
     }
 
@@ -26,7 +108,7 @@ namespace IMJunior
     {
         public string ShowForm()
         {
-            Console.WriteLine("Мы принимаем: QIWI, WebMoney, Card");
+            Console.WriteLine("Мы принимаем: QIWI, WebMoney, Card, YobaPay");
 
             //симуляция веб интерфейса
             Console.WriteLine("Какое системой вы хотите совершить оплату?");
@@ -36,18 +118,14 @@ namespace IMJunior
 
     public class PaymentHandler
     {
-        public void ShowPaymentResult(string systemId)
+        public void ShowPaymentResult(IPaymentSystem paymentSystem)
         {
-            Console.WriteLine($"Вы оплатили с помощью {systemId}");
+            Console.WriteLine($"Вы оплатили с помощью {paymentSystem}");
 
-            if (systemId == "QIWI")
-                Console.WriteLine("Проверка платежа через QIWI...");
-            else if (systemId == "WebMoney")
-                Console.WriteLine("Проверка платежа через WebMoney...");
-            else if (systemId == "Card")
-                Console.WriteLine("Проверка платежа через Card...");
+            paymentSystem.ExecutePayment();
 
-            Console.WriteLine("Оплата прошла успешно!");
+            if (paymentSystem.IsPaymentExecuted())
+                Console.WriteLine("Оплата прошла успешно!");
         }
     }
 }
